ea186351d0389998c0784fb7bc889d352582358a
Arslan Faisal <arslanfaisal6300@hotmail.com>
2026-03-09 04:07:36 +0500

    bnbn

--- .next/dev/server/app/api/queries/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/queries/route.js")
-R.c("server/chunks/node_modules_next_681ac732._.js")
-R.c("server/chunks/[root-of-the-server]__92759818._.js")
-R.c("server/chunks/_next-internal_server_app_api_queries_route_actions_e2153576.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/queries/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/queries/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"M7T54WxJUp8qmaxl9se1MC0lLw2g7yzcfjBoq33iaE0=\"\n}"
